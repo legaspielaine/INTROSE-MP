95c6a5fc4050e6d504b9c35ccfa609972785b048
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2017-10-25 22:26:20 +0800

    new changes

--- liamfoodcorp/acctsys/models.py
+++ liamfoodcorp/acctsys/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 # Create your models here.
+class Branch(models.Model):
+    branchName = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    defBank = models.CharField(max_length=50)
+    
+    def _str_(self):
+        return str(branchName)
+    
 class ChartOfAccounts(models.Model):
     accGrpentry = models.CharField(max_length=50)
     accCode = models.IntegerField(default=0)
     accDescrip = models.CharField(max_length=50)
+    branch = models.ManyToManyField(Branch)
     Debit = 'Debit'
     Credit = 'Credit'
     TYPES_ENTRY =(
@@ -18,6 +27,7 @@ class ChartOfAccounts(models.Model):
         return "ChartAcct - " + str(self.id)
     
 class ProductList(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     prodCategory = models.CharField(max_length=50) #productList like dimsum, mami, etc.
     
     def _str_(self):
@@ -53,6 +63,7 @@ class VoucherInfo(models.Model):
     
     
 class JV(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     pubDateJV = models.DateTimeField(auto_now_add=True)
     remarks = models.CharField(max_length=150)
     totalAmt =  models.DecimalField(max_digits=10, decimal_places=2) #computed fixed amount not editable by user 
@@ -62,6 +73,7 @@ class JV(models.Model):
         return "JV - " + str(self.id)
     
 class CDV(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     payee = models.CharField(max_length=30)
     checkAmt = models.DecimalField(max_digits=10, decimal_places=2)
     checkNo = models.CharField(max_length=30)
@@ -74,49 +86,40 @@ class CDV(models.Model):
         return "CDV - " + str(self.id)
     
 class CRV(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     receivedFr = models.CharField(max_length=20)
     pubDateCRV = models.DateTimeField(auto_now_add=True)
     remarks = models.CharField(max_length=150)
     totalAmt =  models.DecimalField(max_digits=10, decimal_places=2) #computed fixed amount not editable by user 
     vInfo = models.ManyToManyField(VoucherInfo)
     
-
-class Branch(models.Model):
-    branchName = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    defaultBank = models.ForeignKey(ChartOfAccounts, on_delete=models.CASCADE)
-    prodList = models.ManyToManyField(ProductList)
-    listJV = models.ManyToManyField(JV)
-    listCDV = models.ManyToManyField(CDV)
-    listCRV = models.ManyToManyField(CRV)
-    
     def _str_(self):
-        return str(branchName)
-    
+        return "CRV - " + str(self.id)
+
 class Sales(models.Model):
-	branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-	coa = models.ForeignKey(ChartOfAccounts, on_delete=models.CASCADE)
-	crv = models.ForeignKey(CRV, on_delete=models.CASCADE)
-	crew = models.ForeignKey(Crew, on_delete=models.CASCADE)
-	beginBal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	incoming = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	availBal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	out = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	endBal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	actualCount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	shortOrOver = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	salesAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	purchaseAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	shortOrOverAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	discount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	vat = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	netAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	cashOnHand = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	cashInBank = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-	remarks = models.CharField(max_length=200)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    coa = models.ForeignKey(ChartOfAccounts, on_delete=models.CASCADE)
+    crv = models.ForeignKey(CRV, on_delete=models.CASCADE)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE)
+    beginBal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    incoming = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    availBal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    out = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    endBal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    actualCount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    shortOrOver = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    salesAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    purchaseAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    shortOrOverAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    discount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    vat = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    netAmt = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    cashOnHand = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    cashInBank = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    remarks = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
 
-	date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "Sales" + str(self.id)  
 
-	def __str__(self):
-		return str(self.id)
 		
