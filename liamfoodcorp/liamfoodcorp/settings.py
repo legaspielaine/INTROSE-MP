3e05a63a43fc759e83a4cac9a360b15c2ed872e3
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2017-10-26 23:27:20 +0800

    add settings session

--- liamfoodcorp/liamfoodcorp/settings.py
+++ liamfoodcorp/liamfoodcorp/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_FINDERS = [
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
