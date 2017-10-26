ea7b7c18633686ebc2242370ceec552dc47297c9
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2017-10-26 14:37:51 +0800

    fixed

--- liamfoodcorp/acctsys/forms.py
+++ liamfoodcorp/acctsys/forms.py
@@ -11,9 +11,9 @@ class BranchForm(forms.ModelForm):
             'defBank',
         ]
         widgets = {
-            'branchName': forms.TextInput(attrs=('required': True,'placeholder':'Enter branch name')),
-            'address': forms.TextInput(attrs=('placeholder':'Enter branch address')),
-            'defBank': forms.TextInput(attrs=('required': True,'placeholder':'Enter default bank')),
+            'branchName': forms.TextInput(attrs={'required':True,'placeholder':'Enter branch name'}),
+            'address': forms.TextInput(attrs={'placeholder':'Enter branch address'}),
+            'defBank': forms.TextInput(attrs={'required':True,'placeholder':'Enter default bank'}),
             
         }
         
@@ -22,7 +22,7 @@ class BranchForm(forms.ModelForm):
             'address':'Address : ',
             'defBank': 'Default Bank : ',
         }
-class COAForm(form.ModelForm):
+class COAForm(forms.ModelForm):
     class Meta:
         model = ChartOfAccounts
         exclude = [
@@ -35,20 +35,20 @@ class COAForm(form.ModelForm):
             'accEntry', 
         ]
         
-        widgets = [
+        widgets = {
             
-            'accGrpentry': forms.TextInput(attrs=( 'required': True,'placeholder':'Enter branch name')),
-            'accCode': forms.TextInput(attrs=('required': True,'placeholder':'Enter branch name')),
-            'accDescrip': forms.TextInput(attrs=('required': True,'placeholder':'Enter account Description')), 
-            'accEntry': forms.TextInput(attrs=('required': True,'placeholder':'Enter account entry ')),  
-        ]
+            'accGrpentry': forms.TextInput(attrs={'required':True,'placeholder':'Enter branch name'}),
+            'accCode': forms.TextInput(attrs={'required':True,'placeholder':'Enter branch name'}),
+            'accDescrip': forms.TextInput(attrs={'required':True,'placeholder':'Enter account Description'}), 
+            'accEntry': forms.TextInput(attrs={'required':True,'placeholder':'Enter account entry '}),  
+        }
         
-        label = [
+        label = {
             'accGrpentry': 'Account Group Entry', 
             'accCode': 'Account Code',
             'accDescrip': ' Account Description', 
             'accEntry':'Account Entry (Debit/Credit)', 
-        ]
+        }
         
         
 class ProductForm:
@@ -62,7 +62,7 @@ class ProductForm:
             'purchaseAmt',
             'actualStock', 
         ]
-        widgets = [
+        widgets = {
             'grpcode': forms.TextInput(attrs={'required': True, 'placeholder':'Enter grpcode'}),
             'prodCode': forms.NumberInput(attrs={'required': True,'placeholder':'Enter prodcode'}), 
             'prodDescrip': forms.TextInput(attrs={'required': True, 'placeholder':'Enter grpcode'}), 
@@ -70,13 +70,13 @@ class ProductForm:
             'purchaseAmt':forms.NumberInput(attrs={'step': 0.01, 'value':'0.00', 'placeholder':'0.00'}), 
             'actualStock':forms.NumberInput(attrs={'step': 0.01, 'value':'0.00', 'placeholder':'0.00'}),  
             
-        ]
-        label = [
+        }
+        label = {
             'grpcode':'Group Code : ',
             'prodCode': 'Product Code : ',
             'prodDescrip': 'Product Description : ', 
             'sellingPrice':'Selling Price : ', 
             'purchaseAmt':'Purchase Amount : ',
             'actualStock': 'Actual Stock : ', 
-        ]
+        }
         
